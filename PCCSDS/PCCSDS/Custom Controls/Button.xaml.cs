e59ad7bb5cb4ca2badd9a7d8a655ef3635b571a2
Chamuth Chamandana <c.chamandana@gmail.com>
2016-11-13 12:01:47 +0530

    Modified the User Interface a lot

---
+++ PCCSDS/PCCSDS/Custom Controls/Button.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PCCSDS
+{
+	/// <summary>
+	/// Interaction logic for Button.xaml
+	/// </summary>
+	public partial class Button : UserControl
+	{
+		public Button()
+		{
+			InitializeComponent();
+			
+			UpdateButton();
+		}
+
+		public enum ButtonType
+		{
+			Primary,
+			Secondary,
+			Warning
+		}
+
+		private ButtonType _type;
+
+		/// <summary>
+		/// Type of the button
+		/// </summary>
+		public ButtonType Type
+		{
+			get
+			{
+				return _type;
+			}
+			set
+			{
+				_type = value;
+				UpdateButton();
+			}
+		}
+
+		private string _text;
+
+		/// <summary>
+		/// The text that should be appeared on the Button
+		/// </summary>
+		public string Text
+		{
+			get
+			{
+				return _text;
+			}
+			set
+			{
+				_text = value;
+				UpdateButton();
+			}
+		}
+
+		private void UpdateButton()
+		{
+			TextContent.Content = Text;
+			TextContent.FontFamily = FontFamily;
+			TextContent.FontSize = FontSize;
+
+
+			switch (Type)
+			{
+				case ButtonType.Primary:
+					RectangularContent.Fill = new SolidColorBrush(Colors.Maroon);
+					TextContent.Foreground = new SolidColorBrush(Colors.White);
+					break;
+				case ButtonType.Secondary:
+					RectangularContent.Fill = new SolidColorBrush(Colors.White);
+					TextContent.Foreground = new SolidColorBrush(Colors.Maroon);
+					break;
+				case ButtonType.Warning:
+					RectangularContent.Fill = new SolidColorBrush(Colors.Red);
+					TextContent.Foreground = new SolidColorBrush(Colors.White);
+					break;
+			}
+		}
+
+		private void RectangularContent_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			DoubleAnimation OpacityAnimation = new DoubleAnimation();
+			OpacityAnimation.From = RectangularContent.Opacity;
+			OpacityAnimation.To = 0.8d;
+			OpacityAnimation.Duration = TimeSpan.FromMilliseconds(200);
+
+			RectangularContent.BeginAnimation(OpacityProperty, OpacityAnimation);
+
+			//This line of code is very stupid
+			//Circulator.Margin = new Thickness(Mouse.GetPosition(this).X, Mouse.GetPosition(this).Y, Mouse.GetPosition(this).X, Mouse.GetPosition(this).Y);
+
+			DoubleAnimation circulator_dissappearing = new DoubleAnimation();
+			circulator_dissappearing.From = Circulator.Opacity;
+			circulator_dissappearing.To = 1;
+			circulator_dissappearing.Duration = TimeSpan.FromMilliseconds(100);
+			circulator_dissappearing.EasingFunction = new ExponentialEase();
+
+			Circulator.BeginAnimation(OpacityProperty, circulator_dissappearing);
+
+			DoubleAnimation increment = new DoubleAnimation(0, 200, new Duration(TimeSpan.FromMilliseconds(800)));
+			increment.EasingFunction = new QuarticEase();
+			Circulator.BeginAnimation(WidthProperty, increment);
+			Circulator.BeginAnimation(HeightProperty, increment);
+		}
+
+		private void RectangularContent_MouseLeave(object sender, MouseEventArgs e)
+		{
+			DoubleAnimation OpacityAnimation = new DoubleAnimation();
+			OpacityAnimation.From = RectangularContent.Opacity;
+			OpacityAnimation.To = 1;
+			OpacityAnimation.Duration = TimeSpan.FromMilliseconds(200);
+
+			RectangularContent.BeginAnimation(OpacityProperty, OpacityAnimation);
+
+			DoubleAnimation circulator_dissappearing = new DoubleAnimation();
+			circulator_dissappearing.From = Circulator.Opacity;
+			circulator_dissappearing.To = 0;
+			circulator_dissappearing.Duration = TimeSpan.FromMilliseconds(400);
+			circulator_dissappearing.EasingFunction = new ExponentialEase();
+
+			Circulator.BeginAnimation(OpacityProperty, circulator_dissappearing);
+		}
+
+		private void RectangularContent_MouseUp(object sender, MouseButtonEventArgs e)
+		{
+			DoubleAnimation OpacityAnimation = new DoubleAnimation();
+			OpacityAnimation.From = RectangularContent.Opacity;
+			OpacityAnimation.To = 1;
+			OpacityAnimation.Duration = TimeSpan.FromMilliseconds(200);
+
+			RectangularContent.BeginAnimation(OpacityProperty, OpacityAnimation);
+
+			DoubleAnimation circulator_dissappearing = new DoubleAnimation();
+			circulator_dissappearing.From = Circulator.Opacity;
+			circulator_dissappearing.To = 0;
+			circulator_dissappearing.Duration = TimeSpan.FromMilliseconds(400);
+			circulator_dissappearing.EasingFunction = new ExponentialEase();
+
+			Circulator.BeginAnimation(OpacityProperty, circulator_dissappearing);
+		}
+	}
+}
--- PCCSDS/PCCSDS/Start.xaml.cs
+++ PCCSDS/PCCSDS/Start.xaml.cs
@@ -12,45 +12,102 @@ namespace PCCSDS
 		public Start()
 		{
 			InitializeComponent();
-		}
-
-		/// <summary>
-		/// Starts the loading animations of the window even in Windows XP.
-		/// </summary>
-		private void StartAnimations()
-		{
-
-			//Initialize the loading animations
-			DoubleAnimation OpAnim = new DoubleAnimation();
-			OpAnim.From = 0;
-			OpAnim.To = 1;
-			OpAnim.Duration = TimeSpan.FromMilliseconds(1000);
-			OpAnim.EasingFunction = new QuinticEase();
-
-			StartWindow.BeginAnimation(OpacityProperty, OpAnim);
-
-			DoubleAnimation da = new DoubleAnimation();
-			da.From = StartWindow.Width;
-			da.To = StartWindow.Width + 200;
-			da.Duration = TimeSpan.FromMilliseconds(1000);
-			da.EasingFunction = new QuinticEase();
-
-			StartWindow.BeginAnimation(WidthProperty, da);
-
-			DoubleAnimation da2 = new DoubleAnimation();
-			da2.From = StartWindow.Left;
-			da2.To = StartWindow.Left - 100;
-			da2.Duration = TimeSpan.FromMilliseconds(1000);
-			da2.EasingFunction = new QuinticEase();
-
-			StartWindow.BeginAnimation(LeftProperty, da2);
-
+			LoginGrid.Margin = new Thickness(StartWindow.Width, 0, 0, 0);
 		}
 
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
-			//Start the Window Animations
-			StartAnimations(); //QuinticEase looks awesome and looks just like Windows 8.1 and highe animations (especially in Window 10)
+			ThicknessAnimation ti = new ThicknessAnimation();
+			ti.From = new Thickness(StartWindow.Width, 0, 0, 0);
+			ti.To = new Thickness(405, 0, 0, 0);
+			ti.BeginTime = TimeSpan.FromMilliseconds(200);
+			ti.Duration = TimeSpan.FromSeconds(1);
+			ti.EasingFunction = new QuinticEase();
+
+			LoginGrid.BeginAnimation(MarginProperty, ti);
+		}
+
+		private void contactAdmin_btn_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			//Ultra Admin Contact Methods
+			System.Diagnostics.Process.Start("http://www.facebook.com/c.chamandana");
+		}
+
+		private void CreateAccount_btn_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			CreateAccountGrid.Margin = new Thickness(405, 0, 0, 0);
+
+			DoubleAnimation da = new DoubleAnimation();
+			da.From = 0;
+			da.To = 1;
+			da.Duration = TimeSpan.FromSeconds(1);
+			da.EasingFunction = new QuinticEase();
+
+			SignUpCancelButton.IsEnabled = true;
+
+			CreateAccountGrid.BeginAnimation(OpacityProperty, da);
+		}
+
+		private void Login_btn_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			LoginAccountGrid.Margin = new Thickness(405, 0, 0, 0);
+
+			DoubleAnimation da = new DoubleAnimation();
+			da.From = 0;
+			da.To = 1;
+			da.Duration = TimeSpan.FromSeconds(1);
+			da.EasingFunction = new QuinticEase();
+
+			LoginCancelButton.IsEnabled = true;
+
+			LoginAccountGrid.BeginAnimation(OpacityProperty, da);
+		}
+
+		private void Button_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			DoubleAnimation da = new DoubleAnimation();
+			da.From = 1;
+			da.To = 0;
+			da.Duration = TimeSpan.FromSeconds(1);
+			da.EasingFunction = new QuinticEase();
+			da.Completed += Da_Completed;
+
+			LoginCancelButton.IsEnabled = false;
+
+			LoginAccountGrid.BeginAnimation(OpacityProperty, da);
+		}
+
+		private void Da_Completed(object sender, EventArgs e)
+		{
+			LoginAccountGrid.Margin = new Thickness(StartWindow.Width, 0, 0, 0);
+			CreateAccountGrid.Margin = new Thickness(StartWindow.Width, 0, 0, 0);
+
+		}
+
+		private void SignUpCancelButton_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+
+			DoubleAnimation da = new DoubleAnimation();
+			da.From = 1;
+			da.To = 0;
+			da.Duration = TimeSpan.FromSeconds(1);
+			da.EasingFunction = new QuinticEase();
+			da.Completed += Da_Completed;
+
+			SignUpCancelButton.IsEnabled = false;
+
+			CreateAccountGrid.BeginAnimation(OpacityProperty, da);
+		}
+
+		private void LogInButton_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			//Log in to the account
+		}
+
+		private void SignUpBtn_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			//Create a new account
+
 		}
 	}
 }
