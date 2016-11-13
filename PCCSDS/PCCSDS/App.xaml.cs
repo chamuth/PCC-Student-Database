8c7fbe9bc3599a2601f2caf856f8e33f79df9fc2
Chamuth Chamandana <c.chamandana@gmail.com>
2016-11-13 08:23:23 +0530

    Initialized the Repository / Project: Added all of the files to the project

---
+++ PCCSDS/PCCSDS/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PCCSDS
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		/// <summary>
+		/// Occurs in the start of the process / application instance.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+				
+		}
+	}
+}
