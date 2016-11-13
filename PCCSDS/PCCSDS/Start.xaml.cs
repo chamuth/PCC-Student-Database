68acaf9ff86dd5720c8b43ba6d778e9b2bdd4759
Chamuth Chamandana <c.chamandana@gmail.com>
2016-11-13 08:49:08 +0530

    Modified some content in the Start window

--- PCCSDS/PCCSDS/Start.xaml.cs
+++ PCCSDS/PCCSDS/Start.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace PCCSDS
 {
@@ -22,6 +12,45 @@ namespace PCCSDS
 		public Start()
 		{
 			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Starts the loading animations of the window even in Windows XP.
+		/// </summary>
+		private void StartAnimations()
+		{
+
+			//Initialize the loading animations
+			DoubleAnimation OpAnim = new DoubleAnimation();
+			OpAnim.From = 0;
+			OpAnim.To = 1;
+			OpAnim.Duration = TimeSpan.FromMilliseconds(1000);
+			OpAnim.EasingFunction = new QuinticEase();
+
+			StartWindow.BeginAnimation(OpacityProperty, OpAnim);
+
+			DoubleAnimation da = new DoubleAnimation();
+			da.From = StartWindow.Width;
+			da.To = StartWindow.Width + 200;
+			da.Duration = TimeSpan.FromMilliseconds(1000);
+			da.EasingFunction = new QuinticEase();
+
+			StartWindow.BeginAnimation(WidthProperty, da);
+
+			DoubleAnimation da2 = new DoubleAnimation();
+			da2.From = StartWindow.Left;
+			da2.To = StartWindow.Left - 100;
+			da2.Duration = TimeSpan.FromMilliseconds(1000);
+			da2.EasingFunction = new QuinticEase();
+
+			StartWindow.BeginAnimation(LeftProperty, da2);
+
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			//Start the Window Animations
+			StartAnimations(); //QuinticEase looks awesome and looks just like Windows 8.1 and highe animations (especially in Window 10)
 		}
 	}
 }
